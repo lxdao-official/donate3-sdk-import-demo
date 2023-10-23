f9ddbc4aa42c015562b4f7e62694d86ff3e8aaf6
newraina <newraina@users.noreply.github.com>
2023-10-23 09:45:07 +0000

    use svgr

--- import/nextjs/next.config.js
+++ import/nextjs/next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["donate3-sdk"],
+  webpack(config) {
+// Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: [{ loader: '@svgr/webpack', options: { exportType: 'named' } }],
+      },
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
 }
 
 module.exports = nextConfig
